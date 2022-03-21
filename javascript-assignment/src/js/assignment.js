d5b352d64aab3a619bf678a43e32f8c60ef6ef85
Tolohuntomilade <tomiladekareem@gmail.com>
2022-03-21 15:05:00 +0100

    Addition of features for the forms

---
+++ javascript-assignment/src/js/assignment.js
@@ -0,0 +1,11 @@
+//QUESTION 1
+
+    var firstNumber = 6;
+    var secondNumber = 10;
+    var biggerNumber = firstNumber;
+            if (secondNumber > firstNumber)
+            {
+                biggerNumber = secondNumber;
+            }
+    console.log();("The bigger number is: {0}", biggerNumber);
+
